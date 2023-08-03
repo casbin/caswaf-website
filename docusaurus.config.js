5c0646cf097380293c0dd8600d3b6fab36bf989b
YunShu <im@yunshu.site>
2023-08-03 21:26:10 +0800

    fix: 404 issue
    https://github.com/casbin/casbin-website-v2/pull/200

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,6 @@ module.exports = {
   favicon: "img/favicon.png",
   organizationName: "casbin", // Usually your GitHub org/user name.
   projectName: "caswaf-website", // Usually your repo name.
-  trailingSlash: false,
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh", "fr", "de", "ko", "ru", "ja"],
